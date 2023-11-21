b5b32b43f026c69827193fea59922cf11c4de6f0
YaBoiHunta <letoucan69420@gmail.com>
2023-11-20 16:37:03 -0800

    Updated game resources and upgrades in Idle Game HTML and JavaScript files

--- js/Idlegame.js
+++ js/Idlegame.js
@@ -6,40 +6,66 @@ class IdleGame {
             wood: 0,
             food: 0
         };
+        this.upgrades = {
+            stone: 1,
+            water: 1,
+            wood: 1,
+            food: 1
+        };
+        this.upgradeCosts = {
+            stone: 10,
+            water: 10,
+            wood: 10,
+            food: 10
+        };
+        
+        
+
     }
 
     mine() {
-        this.resources.stone++;
+        this.resources.stone += this.upgrades.stone;
         document.getElementById('stoneCount').textContent = this.resources.stone;
     }
 
     gatherWater() {
-        this.resources.water++;
+        this.resources.water += this.upgrades.water;
         document.getElementById('waterCount').textContent = this.resources.water;
     }
 
     chop() {
-        this.resources.wood++;
+        this.resources.wood += this.upgrades.wood;
         document.getElementById('woodCount').textContent = this.resources.wood;
     }
 
     hunt() {
-        this.resources.food++;
+        this.resources.food += this.upgrades.food;
         document.getElementById('foodCount').textContent = this.resources.food;
+    }
+
+      upgradeResource(resource) {
+        if (this.resources[resource] >= this.upgradeCosts[resource]) {
+            this.resources[resource] -= this.upgradeCosts[resource];
+            this.upgrades[resource]++;
+            this.upgradeCosts[resource] *= 2; // Increase the cost for the next upgrade
+            document.getElementById(`${resource}UpgradeCount`).textContent = this.upgrades[resource];
+            document.getElementById(`${resource}UpgradeCost`).textContent = this.upgradeCosts[resource];
+        } else {
+            alert(`You need ${this.upgradeCosts[resource]} ${resource} to upgrade!`);
+        }
     }
 
     getResources() {
         return this.resources;
     }
+
     updateResourceDisplay() {
         document.getElementById('stoneCount').textContent = this.resources.stone;
         document.getElementById('waterCount').textContent = this.resources.water;
         document.getElementById('woodCount').textContent = this.resources.wood;
         document.getElementById('foodCount').textContent = this.resources.food;
     }
-    
 }
-
 
 // Usage:
 const game = new IdleGame();
@@ -60,3 +86,20 @@ document.getElementById('huntButton').addEventListener('click', () => {
     game.hunt();
     game.updateResourceDisplay();
 });
+
+document.getElementById('stoneUpgradeButton').addEventListener('click', () => {
+    game.upgradeResource('stone');
+    game.updateResourceDisplay();
+});
+document.getElementById('waterUpgradeButton').addEventListener('click', () => {
+    game.upgradeResource('water');
+    game.updateResourceDisplay();
+});
+document.getElementById('woodUpgradeButton').addEventListener('click', () => {
+    game.upgradeResource('wood');
+    game.updateResourceDisplay();
+});
+document.getElementById('foodUpgradeButton').addEventListener('click', () => {
+    game.upgradeResource('food');
+    game.updateResourceDisplay();
+}); 
